937309007b69712788ca61053231b7fd6d3bb6bf
Shashvat Jhaveri <shash@shashvatsmbpnew.lan>
2023-09-29 01:22:23 -0400

    init gradle

---
+++ lib/src/main/java/shash_devopstest/jhaveri/shashvat/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package shash_devopstest.jhaveri.shashvat.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
